1e547bd0fc70e4ae3b5fd239d3a67eebe198c622
Vuk Perovic <perovicv515@gmail.com>
2019-05-23 20:34:39 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -3,25 +3,15 @@ var animal = 'dog'
 function myAnimal() {
  return animal
 }
-
 function yourAnimal() {
  var animal = 'cat'
   return animal
 }
-
 function add2(n) {
 var two = 2
  return n + two
-
- 
 }
-
 var funkyFunction = function() {
   return function() {
     return "FUNKY!"
-  }
-}
-
-// We want to set theFunk equal to "FUNKY!" using our funkyFunction.
-// NOTE: you only need to modify the code below this line.
-var theFunk = "FUNKY!"
+var animal
